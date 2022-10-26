13ab8262ada6bef0c8ff8f7e74ee83618228eb0b
Preston Chanta <prestonchanta@gmail.com>
2022-10-26 02:36:07 -0500

    tier 1 done. check just incase.

--- EECS678_Quash/quash.c
+++ EECS678_Quash/quash.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <strings.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/wait.h>
 #include <termios.h>
@@ -12,12 +12,62 @@ int terminal = STDIN_FILENO;
 int is_interactive;
 static struct termios parent_tmodes;
 char input; 
-static char* myargv[15];
-static int myargc = 0;
+static char* argv[15];
+static int argc = 0;
+int buffer_char = 0;
+char buffer[100];
 
 void printBash()
 {
     printf("[BASH]$ ");
+}
+
+// puts the line in argv var
+void readLine()
+{
+    while(argc != 0)
+    {
+        argv[argc] = NULL;
+        argc--;
+    }
+    buffer_char = 0;
+    char *buff;
+    while(input != '\n')
+    {
+        buffer[buffer_char++] = input;
+        input = getchar();
+    }
+    buffer[buffer_char] = 0x00;
+    buff = strtok(buffer, " ");
+    while(buff != NULL)
+    {
+        argv[argc] = buff;
+        buff = strtok(NULL, " ");
+        argc++;
+    }
+}
+
+void doCmd()
+{
+    if(strcmp("exit",argv[0]) ==  0 || strcmp("quit",argv[0]) ==  0)
+        exit(3);
+    else
+    {
+        pid_t pid;
+        pid = fork();
+        if(pid == 0)
+        {
+            setpgrp();
+            // does the command (echo $HOME and echo $PWD don't work)
+            if(execvp(*argv, argv) == -1)
+                perror(" ");
+                exit(0);
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
 }
 
 int main()
@@ -26,17 +76,21 @@ int main()
     parent_pgid = getpgrp();
     terminal = STDIN_FILENO;
     is_interactive = isatty(terminal);
-
+    
+    printf("Welcome...\n");
+/*
     if(is_interactive)
     {
         while(tcgetpgrp(terminal) != (parent_pgid))
             kill(parent_pid,SIGTTIN);
+
         signal(SIGQUIT, SIG_IGN);
         signal(SIGTTOU, SIG_IGN);
         signal(SIGTTIN, SIG_IGN);
         signal(SIGTSTP, SIG_IGN);
         signal(SIGINT, SIG_IGN);
-        //signal(SIGCHLD, &signalHandler_child);
+        signal(SIGCHLD, &signalHandler_child);
+
         setpgid(parent_pid,parent_pid);
         parent_pgid = getpgrp();
         
@@ -44,6 +98,7 @@ int main()
             tcgetattr(terminal,&parent_tmodes);
         char* curr_dir = (char*)calloc(1024,sizeof(char));
     }
+*/
     printBash();
     fflush(stdout);
     while(1)
@@ -53,6 +108,8 @@ int main()
             printBash();
         else
         {
+            readLine();
+            doCmd();
             printBash();
         }
     }
