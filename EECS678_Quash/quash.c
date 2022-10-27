27ad52eb192e8dc3e3d75e88bf257d3c8b1b2da6
BayleyD <Bayley.D021001@gmail.com>
2022-10-26 20:37:19 -0500

    Update quash.c

--- EECS678_Quash/quash.c
+++ EECS678_Quash/quash.c
@@ -1,3 +1,9 @@
+//TO DO, add authors and stuff.
+
+/******************************************************************************************************/
+//Initialize libraries and some variables
+
+//Libraries
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -6,6 +12,7 @@
 #include <sys/wait.h>
 #include <termios.h>
 
+//Global Variables
 pid_t parent_pid;
 pid_t parent_pgid;
 int terminal = STDIN_FILENO;
@@ -16,68 +23,199 @@ static char* argv[15];
 static int argc = 0;
 int buffer_char = 0;
 char buffer[100];
+char* curr_dir;
 
-void printBash()
+/******************************************************************************************************/
+//Basic functions
+//This includes QOL and some built-in commands to Quash
+
+//Simply print out the quash terminal
+void printQuash()
 {
-    printf("[BASH]$ ");
+    printf("[QUASH]$ ");
 }
 
-// puts the line in argv var
-void readLine()
-{
-    while(argc != 0)
-    {
+//Creates a process to clear out terminal
+void clear_sky(){
+    pid_t status = fork();
+    if( status == 0 ){
+        if( execvp( "clear", (char* const[]){"clear", NULL} ) == -1 ){
+        printf( "error" );
+        exit(0);
+        }
+    }
+}
+
+//Implements echo commmand
+void doEcho(){
+    //Prints out current path working directory
+    if( argv[ 1 ] == getenv( "PWD" ) ){
+        printf( "%s\n", curr_dir );
+    }
+    //TO DO add remaining echo functionality
+}
+
+//Implements export command
+void doExport(){
+    
+}
+
+//Implements pwd commmand
+void doPWD(){
+    if( strcmp( "pwd", argv[0] ) == 0 ){
+        printf( "%s\n", curr_dir );
+    }
+}
+
+//Impements the change directory command
+void changeDir(){
+    if( argv[1] == NULL ){
+        chdir( getenv( "HOME" ) );
+    } else {
+        if( chdir( argv[1] ) == -1  ){
+            printf( "%s: No such directory\n", argv[1]);
+        }
+    }
+    curr_dir = getenv( "PWD" );
+}
+
+/******************************************************************************************************/
+
+//Creates a job (another procees)
+void createJob(){
+    pid_t pid;
+    pid = fork();
+    if(pid == 0){
+        setpgrp();
+        if ( execvp( *argv, argv ) == -1 ){
+        perror(" ");
+        exit(0);
+        }
+    } else {
+        wait(NULL);
+    }
+}
+
+/******************************************************************************************************/
+//Parser functions, determine what to do based on inputs
+
+//Replaces any found $PATH found with the corresponding path directory
+void findPath(){
+    char* str;
+    char* token;
+    int len = 0;
+    for( int i = 0; i < argc; i ++ ){
+        str = argv[i];
+        len = strlen( str );
+        token = strtok( str, "$" );
+        for( int j = 0; j < len; j ++ ){
+            if( getenv( str + j ) ){
+                str = token;
+                if( getenv( str ) ){ 
+                    argv[ i ] = getenv( argv[i] + j );
+                } else {
+                    strcat( str , getenv( argv[i] + j ) );
+                    argv[ i ] = str;
+                }
+            }
+        }
+    }
+}
+
+//Parses the command given in argv
+int parseCommand(){ 
+    findPath();
+    //Does the echo command
+    /*if( strcmp( "echo", argv[0] ) == 0 ){
+        doEcho();
+        return( 1 );
+    }*/
+    //Does the pwd commands
+    if( strcmp( "pwd", argv[0] ) == 0 ){
+        doPWD();
+        return( 1 );
+    }
+    //Does export commands
+    if( strcmp( "export", argv[0] ) == 0 ){
+        doExport();
+        return( 1 );
+    }
+    //Change Directory command
+    if( strcmp( "cd", argv[0] ) == 0 ){
+        changeDir();
+        return( 1 );
+    }
+    return( 0 );
+}
+
+//Puts the line in argV
+void readLine(){
+    while(argc != 0){
         argv[argc] = NULL;
         argc--;
     }
     buffer_char = 0;
     char *buff;
-    while(input != '\n')
-    {
+    while(input != '\n'){
         buffer[buffer_char++] = input;
         input = getchar();
     }
     buffer[buffer_char] = 0x00;
     buff = strtok(buffer, " ");
-    while(buff != NULL)
-    {
+    while(buff != NULL){
         argv[argc] = buff;
         buff = strtok(NULL, " ");
         argc++;
     }
 }
 
-void doCmd()
-{
-    if(strcmp("exit",argv[0]) ==  0 || strcmp("quit",argv[0]) ==  0)
+//Determines whether to exit, do built-in commands, or create a new job(process)
+void doCmd(){
+    //The mulitple ways of exiting quash
+    if(strcmp("exit",argv[0]) ==  0 || strcmp("quit",argv[0]) ==  0){
         exit(3);
-    else
-    {
-        pid_t pid;
-        pid = fork();
-        if(pid == 0)
-        {
-            setpgrp();
-            // does the command (echo $HOME and echo $PWD don't work)
-            if(execvp(*argv, argv) == -1)
-                perror(" ");
-                exit(0);
-        }
-        else
-        {
-            wait(NULL);
-        }
+    //If it isn't a built-in command it creates a job
+    } 
+    if( parseCommand() == 0 ) {
+        createJob();
     }
 }
 
-int main()
-{
+/******************************************************************************************************/
+//Main, initializes some things and runs Quash
+
+int main(){
+    //Creates a process to empty terminal
+    clear_sky();
+    wait(NULL);
+
+    //Set some variables
     parent_pid = getpid();
     parent_pgid = getpgrp();
     terminal = STDIN_FILENO;
     is_interactive = isatty(terminal);
-    
-    printf("Welcome...\n");
+    curr_dir = getenv( "PWD" );
+
+    //Moved the large chunk of commented code below
+
+    //Runs Quash
+    printQuash();
+    fflush(stdout);
+    while(1){
+        input = getchar();
+        if(input == '\n'){
+            printQuash();
+        } else {
+            readLine();
+            doCmd();
+            printQuash();
+        }
+    }
+    return 0;
+}
+
+/******************************************************************************************************/
+//Chunk of code
 /*
     if(is_interactive)
     {
@@ -99,20 +237,3 @@ int main()
         char* curr_dir = (char*)calloc(1024,sizeof(char));
     }
 */
-    printBash();
-    fflush(stdout);
-    while(1)
-    {
-        input = getchar();
-        if(input == '\n')
-            printBash();
-        else
-        {
-            readLine();
-            doCmd();
-            printBash();
-        }
-    }
-
-    return 0;
-}
